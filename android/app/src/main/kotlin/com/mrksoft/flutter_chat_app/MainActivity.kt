bd8c9195b766f6c8c2363d5b50d071add17a6ae1
muratrahmikurtul <muratrahmikurtul@gmail.com>
2020-12-21 21:40:59 +0100

    Initialize Project

---
+++ android/app/src/main/kotlin/com/mrksoft/flutter_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mrksoft.flutter_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sign_in_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Chat App'),
+        elevation: 0,
+      ),
+      backgroundColor: Colors.grey.shade200,
+      body: Column(
+        children: <Widget>[
+          Text("")
+        ],
+      ),
+    );
+  }
+}
